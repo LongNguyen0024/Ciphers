a5a1c1468d0633b1c571b2af993c237f1fd9d663
Long Nguyen <Longnguyen0024@gmail.com>
2020-03-14 03:35:20 -0700

    I did my part

--- cipher.py
+++ cipher.py
@@ -1,8 +1,7 @@
 ######################################################################################
 #       To run in command prompt:                                                    #
 #       python cipher.py <CIPHER NAME> <KEY> <ENC/DEC> <INPUTFILE> <OUTPUT FILE>     #
-######################################################################################
-####thisisl
+####################################################################################
 import sys
 
 def main():
@@ -97,12 +96,140 @@ class PlayFair(CipherInterface):                  # PLF
         
     def setKey(self, key):
         self.key = key
+        table = []
+        row = []
+        index = 0
+        alphabet = "abcdefghiklmnopqrstuvwxyz"  #exclude j temporarily
+        key = key.replace(" ", "")
+        key = key + alphabet
+
+        seen = set()
+        while index < len(key):
+            
+            if not key[index] in seen:
+                row.append(key[index])
+                seen.add(key[index])
+                if len(row) == 5:
+                    table.append(row)
+                    row = []
+            index += 1
+        return table
+    
+    def do_pair(plainText):            #plaintext: bolloon
+    index = 0
+    pair = ""
+    result = []
+    while index < len(plainText):
+        if pair == "":
+            pair = plainText[index]
+            index += 1
+
+        elif len(pair) == 1:
+            if pair == plainText[index]:
+                pair = pair + 'x'
+            else:
+                pair = pair + plainText[index]
+                index += 1
         
-    def encrypt(self, plaintext):
-        pass
+        if len(pair) == 2:
+            result.append(pair)
+            pair = ""
+    if len(pair) == 1:
+        pair = pair + 'x'
+
+        result.append(pair)
+    return result
+
+    def encrypt(self, inputFile, outputFile, key):
+        result =""
+        cipherText = []
+        inFile = open(inputFile, "r")
+        outFile = open(outputFile, "w")
+        inputString = inFile.read()
+        plainText = do_pair(inputString)
+        table = setKey(key)
         
-    def decrypt(self, ciphertext):
-        pass
+        m = {}                                #construct coordinate table
+        for tRow in range(len(table)):
+            for tCol in range(len(table)):
+                m[table[tRow][tCol]] = tRow, tCol
+
+        for myWord in plainText:                    #each pair in the new plainText: bo
+            l1 = myWord[0]
+            l2 = myWord[1]
+
+            if l1 in m:
+                (x1, y1) = m.get(l1)
+            if l2 in m:
+                (x2, y2) = m.get(l2)
+
+            Encl1 = ""
+            Encl2 = ""
+            
+            #same row
+            if x1 == x2:
+                Encl1 = (x1, (y1 + 1) % 5)
+                Encl2 = (x2, (y2 + 1) % 5)
+            #same column
+            elif y1 == y2:
+                Encl1 = ((x1 + 1) % 5, y1)
+                Encl2 = ((x2 + 1) % 5, y2)
+            #different row and column
+            else:
+                Encl1 = (x1, y2)
+                Encl2 = (x2, y1)
+
+            cipherText.append(table[Encl1[0]][Encl1[1]] + table[Encl2[0]][Encl2[1]])
+        for i in cipherText:
+            result += i
+        outFile.write(result)
+
+        
+    def decrypt (inputFile, outputFile, key):
+        decryptResult = ""
+        inFile = open(inputFile, "r")
+        outFile = open(outputFile, "w")
+        inputString = inFile.read()
+
+        textToDecrypt = do_pair(inputString)
+        table = setKey(key)
+        
+        decryptedText = []
+        m = {}                                #construct coordinate table
+        for tRow in range(len(table)):
+            for tCol in range(len(table)):
+                m[table[tRow][tCol]] = tRow, tCol
+
+        for myWord in textToDecrypt:                    #each pair in the new plainText: bo
+            l1 = myWord[0]
+            l2 = myWord[1]
+
+            if l1 in m:
+                (x1, y1) = m.get(l1)
+            if l2 in m:
+                (x2, y2) = m.get(l2)
+
+            Encl1 = ""
+            Encl2 = ""
+            
+            #same row
+            if x1 == x2:
+                Encl1 = (x1, y1 - 1)
+                Encl2 = (x2, y2 - 1)
+            #same column
+            elif y1 == y2:
+                Encl1 = (x1 - 1, y1)
+                Encl2 = (x2 - 1, y2)
+            #different row and column
+            else:
+                Encl1 = (x1, y2)
+                Encl2 = (x2, y1)
+            
+            decryptedText.append(table[Encl1[0]][Encl1[1]] + table[Encl2[0]][Encl2[1]])
+        
+        for i in decryptedText:
+            decryptResult += i
+        outFile.write(decryptResult)
 
 class RowTransposition(CipherInterface):          # RTS
     def __init__(self):
@@ -148,7 +275,7 @@ class RowTransposition(CipherInterface):          # RTS
                     table[i].append(plaintext[incrementBy])
                     incrementBy+=cols
 
-        # Piece together the final result into a string    
+        # Piece together the final result into a string
         finalResult = ''
 
         for i in self.key:
@@ -195,7 +322,7 @@ class RowTransposition(CipherInterface):          # RTS
         # Add new table to rearrange columns in order
         new_table = {}
 
-        # Rearrange columns in order 
+        # Rearrange columns in order
         for i in range(1, len(self.key)+1):
             new_table[str(i)]=table[str(i)]
 
@@ -221,68 +348,84 @@ class Railfence(CipherInterface):                 # RFC
     def setKey(self, key):
         self.key = key
         
-    def encrypt(self, plaintext):
-        for i in message:
-            if( i == " "):
-                message = message.replace(i, "")
-        
+    def encrypt(self, inputFile, outputFile, key):
+        #meetmeafterthetogaparty
+        inFile = open(inputFile, "r")
+        outFile = open(outputFile, "w")
+        keyRow = 0
+        plainText = inFile.read()
+
         result = ""
         rowResult = ""
         temp = 0
         keyRow = 0
-        while keyRow < self.key:
-            while temp < len(message):       
-                rowResult = rowResult + message[temp]
-                temp = temp + self.key
+
+        for i in plainText:    #strip all spaces if present
+            if i == " ":
+                plainText = plainText.replace(i, "")
+        
+        while keyRow < key:
+            while temp < len(plainText):
+                rowResult = rowResult + plainText[temp]
+                temp = temp + key
             keyRow = keyRow + 1
             temp = keyRow
             result = result + rowResult
             rowResult = ""
 
-        return (result)
-        
-    def decrypt(self, cipherText):
-        myList = []
-        tempString = cipherText
-        index = 0
-        letterPerRow = int(len(cipherText) / self.key)
-        extraLetters = int(len(cipherText) % self.key)
+        outFile.write(result)
+        inFile.close()
+        outFile.close()
 
-        #create a list of rows of letters
-        while index < self.key:
-            if extraLetters > 0:
-                myList.append(tempString[:letterPerRow + 1])
-                tempString = tempString[letterPerRow+1:]
-                extraLetters -= 1
-            else:
-                myList.append(tempString[:letterPerRow])
-                tempString = tempString[letterPerRow:]
-            index += 1
+def decrypt(self, inputFile, outputFile, key):
+    inFile = open(inputFile, "r")
+    outFile = open(outputFile, "w")
+    plainText = inFile.read()
 
-        row = 0
-        col = 0
-        tempString = ""
-        result = ""
-        extraLetters = int(len(cipherText) % self.key)
-        
-        #start decrypting
-        while col < letterPerRow:
-            while row < self.key:
-                tempString = tempString + myList[row][col]
-                row += 1
-            col += 1
-            row = 0
-            result += tempString
-            tempString = ""
-            
-        #fill up with extra letters
+    myList = []
+    tempString = plainText
+    index = 0
+    letterPerRow = int(len(plainText) / key)
+    extraLetters = int(len(plainText) % key)
+
+    #create a list of rows of letters
+    while index < key:
         if extraLetters > 0:
-            i = 0
-            while i < extraLetters:
-                result += myList[i][letterPerRow]
-                i += 1
+            myList.append(tempString[:letterPerRow + 1])
+            tempString = tempString[letterPerRow+1:]
+            extraLetters -= 1
+        else:
+            myList.append(tempString[:letterPerRow])
+            tempString = tempString[letterPerRow:]
+        index += 1
 
-        return result
+    row = 0
+    col = 0
+    tempString = ""
+    result = ""
+    extraLetters = int(len(plainText) % key)
+    
+    #start decrypting
+    while col < letterPerRow:
+        while row < key:
+            tempString = tempString + myList[row][col]
+            row += 1
+        col += 1
+        row = 0
+        result += tempString
+        tempString = ""
+        
+    #fill up with extra letters
+    if extraLetters > 0:
+        i = 0
+        while i < extraLetters:
+            result += myList[i][letterPerRow]
+            i += 1
+
+    outFile.write(result)
+    inFile.close()
+    outFile.close()
+
 
 class Vigenre(CipherInterface):                   # VIG
     def __init__(self):
